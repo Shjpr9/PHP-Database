4a2f64f247be8433d012e70957129fd18119303d
Shjpr9 <67644313+Shjpr9@users.noreply.github.com>
2022-08-10 09:47:39 +0430

    Update database_public.php

--- database_public.php
+++ database_public.php
@@ -9,7 +9,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // databese structure:
 // id, username, last_message, times_used
-// you should it to your own db structure. 
+// you should change it to your own db structure. 
 // Wherever you see ch_stc, it means you should change the structure  
 
 
